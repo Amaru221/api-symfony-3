d0eeb07e0a4ad8edbfd3d843affca006b2e0bff8
null221 <agd1923@gmail.com>
2025-04-29 16:04:05 +0200

    changed the value of isOwnedByAuthenticatedUser by actual user is equal to owner

--- src/State/DragonTreasureStateProvider.php
+++ src/State/DragonTreasureStateProvider.php
@@ -6,12 +6,13 @@ use App\Entity\DragonTreasure;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use ApiPlatform\Doctrine\Orm\State\ItemProvider;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class DragonTreasureStateProvider implements ProviderInterface
 {
 
-    public function __construct(#[Autowire(service: ItemProvider::class)] private ProviderInterface $itemProvider)
+    public function __construct(private Security $security, #[Autowire(service: ItemProvider::class)] private ProviderInterface $itemProvider)
     {
         
     }
@@ -24,7 +25,7 @@ class DragonTreasureStateProvider implements ProviderInterface
             return $treasure;
         }
 
-        $treasure->setIsOwnedByAuthenticatedUser(true);
+        $treasure->setIsOwnedByAuthenticatedUser($this->security->getUser() === $treasure->getOwner());
         // Retrieve the state from somewhere
         return $treasure;
         //dd($operation);
