12c39621c52b5559872a5b810d33097994ddd4e2
null221 <agd1923@gmail.com>
2025-04-29 15:53:09 +0200

    verify if treasure retrieved is a instance of DragonTreasure. if yes, setIsOwnedByAuthenticatedUser true for test

--- src/State/DragonTreasureStateProvider.php
+++ src/State/DragonTreasureStateProvider.php
@@ -2,9 +2,10 @@
 
 namespace App\State;
 
-use ApiPlatform\Doctrine\Odm\State\ItemProvider;
+use App\Entity\DragonTreasure;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use ApiPlatform\Doctrine\Odm\State\ItemProvider;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class DragonTreasureStateProvider implements ProviderInterface
@@ -18,6 +19,12 @@ class DragonTreasureStateProvider implements ProviderInterface
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
         $treasure = $this->itemProvider->provide($operation, $uriVariables, $context);
+
+        if(!$treasure instanceof DragonTreasure){
+            return $treasure;
+        }
+
+        $treasure->setIsOwnedByAuthenticatedUser(true);
         // Retrieve the state from somewhere
         dd($operation);
     }
