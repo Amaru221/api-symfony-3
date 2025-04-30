90f3d5afbe923574fffbf306a1fcab53dc4dbc50
null221 <agd1923@gmail.com>
2025-04-30 11:50:20 +0200

    stablished processor for POST and PATCH operations

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -15,6 +15,7 @@ use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Serializer\Filter\PropertyFilter;
 use App\Repository\DragonTreasureRepository;
+use App\State\DragonTreasureStateProcessor;
 use App\State\DragonTreasureStateProvider;
 use App\Validator\IsValidOwner;
 use Carbon\Carbon;
@@ -38,9 +39,11 @@ use function Symfony\Component\String\u;
         new GetCollection(),
         new Post(
             security: 'is_granted("ROLE_TREASURE_CREATE")',
+            processor: DragonTreasureStateProcessor::class,
         ),
         new Patch(
             security: 'is_granted("EDIT", object)',
+            processor: DragonTreasureStateProcessor::class,
         ),
         new Delete(
             security: 'is_granted("ROLE_ADMIN")',
