85a7fa0a4f05caee6426a52d3f3767e2b781cdf4
null221 <agd1923@gmail.com>
2025-04-30 16:46:47 +0200

    added new state provider to ApiResource DailyQuest

--- src/ApiResource/DailyQuest.php
+++ src/ApiResource/DailyQuest.php
@@ -3,9 +3,11 @@
 namespace App\ApiResource;
 
 use ApiPlatform\Metadata\ApiResource;
+use App\State\DailyQuestStateProvider;
 
 #[ApiResource(
     shortName: 'Quest',
+    provider: DailyQuestStateProvider::class,
 )]
 class DailyQuest{
 
