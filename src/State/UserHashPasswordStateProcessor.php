8a9cead080f732b6f2a1feab21cbc543caf1a432
null221 <agd1923@gmail.com>
2025-04-30 15:40:31 +0200

    added autowire service PersistProcessor

--- src/State/UserHashPasswordStateProcessor.php
+++ src/State/UserHashPasswordStateProcessor.php
@@ -2,16 +2,18 @@
 
 namespace App\State;
 
+use App\Entity\User;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
-use App\Entity\User;
+use ApiPlatform\Doctrine\Common\State\PersistProcessor;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsDecorator('api_platform.doctrine.orm.state.persist_processor')]
 class UserHashPasswordStateProcessor implements ProcessorInterface
 {
-    public function __construct(private ProcessorInterface $innerProcessor, private UserPasswordHasherInterface $userPasswordHasher)
+    public function __construct(#[Autowire(service: PersistProcessor::class)] private ProcessorInterface $innerProcessor, private UserPasswordHasherInterface $userPasswordHasher)
     {
     }
 
