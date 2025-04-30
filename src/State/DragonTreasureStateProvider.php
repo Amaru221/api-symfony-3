42a3be3e1d34314cc5baafb203c8a8789a35033c
null221 <agd1923@gmail.com>
2025-04-30 11:15:19 +0200

    verify if operation is a collectionOperationProvider

--- src/State/DragonTreasureStateProvider.php
+++ src/State/DragonTreasureStateProvider.php
@@ -7,6 +7,7 @@ use App\Entity\DragonTreasure;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
 use ApiPlatform\Doctrine\Orm\State\ItemProvider;
+use ApiPlatform\Metadata\CollectionOperationInterface;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
@@ -20,6 +21,11 @@ class DragonTreasureStateProvider implements ProviderInterface
 
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
+
+        if($operation instanceof CollectionOperationInterface){
+            return $this->collectionProvider->provide($operation, $uriVariables, $context);
+        }
+
         $treasure = $this->itemProvider->provide($operation, $uriVariables, $context);
 
         if(!$treasure instanceof DragonTreasure){
