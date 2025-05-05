4adea0d9c5f1dfaee95bde29ce10a4c1f8034fef
null221 <agd1923@gmail.com>
2025-05-05 19:42:40 +0200

    hidden day field

--- src/ApiResource/DailyQuest.php
+++ src/ApiResource/DailyQuest.php
@@ -6,6 +6,7 @@ use DateTimeInterface;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use App\State\DailyQuestStateProvider;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
 #[ApiResource(
     shortName: 'Quest',
@@ -13,6 +14,7 @@ use App\State\DailyQuestStateProvider;
 )]
 class DailyQuest{
 
+    #[Ignore]
     public \DateTimeInterface $day;
 
     public function __construct(DateTimeInterface $day){
