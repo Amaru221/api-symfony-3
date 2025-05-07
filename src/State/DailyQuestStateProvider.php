f23041ff939c3d5899ebd052ed836fa26a33c011
null221 <agd1923@gmail.com>
2025-05-07 09:34:43 +0200

    added treasures with repo in provider state

--- src/State/DailyQuestStateProvider.php
+++ src/State/DailyQuestStateProvider.php
@@ -38,6 +38,7 @@ class DailyQuestStateProvider implements ProviderInterface
             $quest->difficultyLevel = $i % 10;
             $quest->status = $i % 2 === 0 ? DailyQuestStatusEnum::ACTIVE : DailyQuestStatusEnum::COMPLETED;
             $quest->lastUpdate = new DateTimeImmutable(sprintf('- %d days', rand(10,100)));
+            $quest->treasures = $this->treasureRepository->findBy([],[],10);
             $quests[$quest->getDayString()] = $quest;
         }
 
