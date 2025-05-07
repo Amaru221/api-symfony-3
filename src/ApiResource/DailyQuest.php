542ddf8bb820ff1fe207f042229cf523c462c79f
null221 <agd1923@gmail.com>
2025-05-07 08:53:20 +0200

    added operations get, getcollection and patch for dailyquest class

--- src/ApiResource/DailyQuest.php
+++ src/ApiResource/DailyQuest.php
@@ -3,14 +3,23 @@
 namespace App\ApiResource;
 
 use DateTimeInterface;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Patch;
+use App\Enum\DailyQuestStatusEnum;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
 use App\State\DailyQuestStateProvider;
+use ApiPlatform\Metadata\GetCollection;
 use Symfony\Component\Serializer\Annotation\Ignore;
 
 #[ApiResource(
     shortName: 'Quest',
     provider: DailyQuestStateProvider::class,
+    operations: [
+        new GetCollection(),
+        new Get(),
+        new Patch(),
+    ]
 )]
 class DailyQuest{
 
