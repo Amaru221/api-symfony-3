c234cb9bc9ce71cb1a15725944407cbefa451284
null221 <agd1923@gmail.com>
2025-05-07 09:27:54 +0200

    added value for property lastUpdate in process method

--- src/State/DailyQuestStateProcessor.php
+++ src/State/DailyQuestStateProcessor.php
@@ -3,13 +3,14 @@ namespace App\State;
 
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use DateTimeImmutable;
 
 class DailyQuestStateProcessor implements ProcessorInterface {
-
 
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
         dd($data);
+        $data->lastUpdate = new DateTimeImmutable('now');
     }
 
 }
