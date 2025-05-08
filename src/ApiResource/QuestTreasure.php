503440a5a7edc3c1cf893cb1cfbc94040917921c
null221 <agd1923@gmail.com>
2025-05-08 10:45:21 +0200

    create new class non ApiResource associated to dailyquest

---
+++ src/ApiResource/QuestTreasure.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\ApiResource;
+
+class QuestTreasure{
+
+
+    public function __construct(
+        public string $name,
+        public int $value,
+        public int $coolFactor,
+    ){
+
+    }
+
+
+}
