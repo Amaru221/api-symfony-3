98d3f0de28da753567df86e8d3127a5584ed6263
null221 <agd1923@gmail.com>
2025-05-08 11:59:15 +0200

    added api property itemsperpage for pagination

--- src/ApiResource/DailyQuest.php
+++ src/ApiResource/DailyQuest.php
@@ -22,6 +22,7 @@ use Symfony\Component\Serializer\Annotation\Ignore;
         new Patch(),
     ],
     processor: DailyQuestStateProcessor::class,
+    paginationItemsPerPage: 10,
 )]
 class DailyQuest{
 
