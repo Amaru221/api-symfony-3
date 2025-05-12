e6b096e7888703c98ae5af032244ec3230bd8b78
null221 <agd1923@gmail.com>
2025-05-12 15:25:11 +0200

    added stateOptions with reference User class

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -2,12 +2,15 @@
 
 namespace App\ApiResource;
 
-use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+use App\Entity\User;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Doctrine\Orm\State\Options;
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
 
 #[ApiResource(
     shortName: 'User',
     provider: CollectionProvider::class,
+    stateOptions: new Options(entityClass: User::class),
 )]
 class UserApi
 {
