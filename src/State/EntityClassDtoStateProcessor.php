4953215c0dc4520fef5162b84b5ea8927c893d36
null221 <agd1923@gmail.com>
2025-05-14 16:37:40 +0200

    add constructor function with repository User. Verifi if data in process is an UserApi, check if is new User or an Update user in mapDtoToEntity

--- src/State/EntityClassDtoStateProcessor.php
+++ src/State/EntityClassDtoStateProcessor.php
@@ -2,12 +2,18 @@
 
 namespace App\State;
 
+use App\Entity\User;
 use App\ApiResource\UserApi;
+use App\Repository\UserRepository;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 
 class EntityClassDtoStateProcessor implements ProcessorInterface
 {
+
+    public function __construct(private UserRepository $userRepository){
+
+    }
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
     {
         // Handle the state
@@ -18,6 +24,15 @@ class EntityClassDtoStateProcessor implements ProcessorInterface
 
     private function mapDtoToEntity(object $userApi){
         assert($userApi instanceof UserApi);
+
+        if($userApi->id)
+        {
+            $entity = $this->userRepository->find($userApi->id);
+            if(!$entity){
+                $entity = new User();
+                
+            }
+        }
 
     }
 }
