6a25f098fe1a508904c1fd25c2ad4e1d1bb92542
null221 <agd1923@gmail.com>
2025-05-14 17:15:01 +0200

    set the id in UserApi resource from user entity

--- src/State/EntityClassDtoStateProcessor.php
+++ src/State/EntityClassDtoStateProcessor.php
@@ -26,6 +26,9 @@ class EntityClassDtoStateProcessor implements ProcessorInterface
         assert($data instanceof UserApi);
         $entity = $this->mapDtoToEntity($data);
         $this->persistProcessor->process($entity, $operation, $uriVariables, $context);
+
+        $data->id = $entity->getId();
+
         return $data;
     }
 
