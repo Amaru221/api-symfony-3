78335901cb458b1e511563cf66d25e2986d55226
null221 <agd1923@gmail.com>
2025-05-14 17:12:41 +0200

    added service PersistProcessor in construct. Input data in our persistProcessor process

--- src/State/EntityClassDtoStateProcessor.php
+++ src/State/EntityClassDtoStateProcessor.php
@@ -2,25 +2,31 @@
 
 namespace App\State;
 
+use ApiPlatform\Doctrine\Common\State\PersistProcessor;
 use App\Entity\User;
 use App\ApiResource\UserApi;
 use App\Repository\UserRepository;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class EntityClassDtoStateProcessor implements ProcessorInterface
 {
 
-    public function __construct(private UserRepository $userRepository){
+    public function __construct(
+        private UserRepository $userRepository,
+        #[Autowire(service: PersistProcessor::class)] private PersistProcessor $persistProcessor,
+    ){
 
     }
 
-    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
         // Handle the state
         assert($data instanceof UserApi);
         $entity = $this->mapDtoToEntity($data);
-        dd($entity);
+        $this->persistProcessor->process($entity, $operation, $uriVariables, $context);
+        return $data;
     }
 
     private function mapDtoToEntity(object $userApi){
