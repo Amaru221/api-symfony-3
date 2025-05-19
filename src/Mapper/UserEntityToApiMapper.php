30292bbb9945e44f5a242ec8151d553024a35ca1
null221 <agd1923@gmail.com>
2025-05-19 14:37:58 +0200

    create mapper class from User to UserApi

---
+++ src/Mapper/UserEntityToApiMapper.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mapper;
+
+use App\Entity\User;
+use App\ApiResource\UserApi;
+use Symfonycasts\MicroMapper\AsMapper;
+use Symfonycasts\MicroMapper\MicroMapperInterface;
+
+#[AsMapper(from: User::class, to: UserApi::class)]
+class UserEntityToApiMapper implements MicroMapperInterface{
+
+
+    public function load(object $from, object $toClass, array $context):object {
+        $entity = $from;
+        assert($entity instanceof User);
+
+        $dto = new UserApi();
+        $dto->id = $entity->getId();
+
+        return $dto;
+
+    }
+
+    public function populate(object $from, object $to, array $context): object {
+        $entity = $from;
+        assert($entity instanceof User);
+        $dto = $to;
+        assert($dto instanceof UserApi);
+
+        $dto->email = $entity->getEmail();
+        $dto->username = $entity->getUsername();
+        $dto->dragonTreasures = $entity->getPublishedDragonTreasures()->getValues();
+        $dto->flameThrowingDistance = rand(1, 10);
+        return $dto;
+    }
+}
