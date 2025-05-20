f78bc1268be4eea8f79d8e73bf1c3992e5660a27
null221 <agd1923@gmail.com>
2025-05-20 16:28:42 +0200

    refactor method mapEntotyToDto. Added resourceClass to do generic for all classes

--- src/State/EntityToDtoStateProvider.php
+++ src/State/EntityToDtoStateProvider.php
@@ -27,14 +27,14 @@ class EntityToDtoStateProvider implements ProviderInterface
     public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
     {
 
-        dd($operation, $context);
+        $resourceClass = $operation->getClass();
         if($operation instanceof CollectionOperationInterface){
             $entities = $this->collectionProvider->provide($operation, $uriVariables, $context);
             assert($entities instanceof Paginator);
 
             $dtos = [];
             foreach($entities as $entity){
-                $dtos [] = $this->mapEntityToDto($entity);
+                $dtos [] = $this->mapEntityToDto($entity, $resourceClass);
             }
 
             return new TraversablePaginator(
@@ -51,14 +51,14 @@ class EntityToDtoStateProvider implements ProviderInterface
             return null;
         }
 
-        return $this->mapEntityToDto($entity);
+        return $this->mapEntityToDto($entity, $resourceClass);
 
     }
 
-    private function mapEntityToDto(object $entity): object
+    private function mapEntityToDto(object $entity, string $resourceClass): object
     {
 
-        return $this->microMapper->map($entity, UserApi::class);
+        return $this->microMapper->map($entity, $resourceClass);
     }
 
 
