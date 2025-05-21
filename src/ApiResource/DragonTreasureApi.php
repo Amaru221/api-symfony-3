950538c3b9df6750d8374696a72f68b2a6222c42
null221 <agd1923@gmail.com>
2025-05-21 19:17:41 +0200

    add validations to some fields

--- src/ApiResource/DragonTreasureApi.php
+++ src/ApiResource/DragonTreasureApi.php
@@ -9,6 +9,9 @@ use App\State\EntityToDtoStateProvider;
 use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiProperty;
 use App\State\EntityClassDtoStateProcessor;
+use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
+use Symfony\Component\Validator\Constraints\LessThanOrEqual;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ApiResource(
     shortName: 'Treasure',
@@ -23,12 +26,17 @@ class DragonTreasureApi
     #[ApiProperty(readable: false, writable:false, identifier: true)]
     public ?int $id = null;
 
+    #[NotBlank()]
     public ?string $name = null;
 
+    #[NotBlank()]
     public ?string $description = null;
 
+    #[GreaterThanOrEqual(0)]
     public int $value = 0;
     
+    #[GreaterThanOrEqual(0)]
+    #[LessThanOrEqual(10)]
     public int $coolFactor = 0;
 
     public ?UserApi $owner = null;
